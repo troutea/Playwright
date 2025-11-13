3c94be1853687d69f15c0c7711018c92aebda3fc
troutea <troutea@yahoo.co.uk>
2025-11-13 13:08:49 +0000

    Initial checkin for Playwright Special locators

---
+++ tests/llc.spec.js
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test';
+
+test('Playwright Special Locators', async ({page}) => {
+
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+
+
+
+
+});
+    
