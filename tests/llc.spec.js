2f516776e6eab83acec438e93030d74f5fa1fcf5
troutea <troutea@yahoo.co.uk>
2025-11-19 14:06:09 +0000

    update with new tests

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -3,6 +3,11 @@ import {test, expect} from '@playwright/test';
 test('Playwright Special Locators', async ({page}) => {
 
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").check();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Female");
+    await page.getByPlaceholder("Password").fill("abc123");
+    await page.getByRole("button", {name: "Submit"}).click();
 
 
 
