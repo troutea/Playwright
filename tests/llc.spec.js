c55845d2c3e9ecbac3f524bf5621cfe5649b49ab
troutea <troutea@yahoo.co.uk>
2025-11-24 14:02:29 +0000

    updating tests

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -8,7 +8,10 @@ test('Playwright Special Locators', async ({page}) => {
     await page.getByLabel("Gender").selectOption("Female");
     await page.getByPlaceholder("Password").fill("abc123");
     await page.getByRole("button", {name: "Submit"}).click();
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("link",{name : "Shop"}).click();
 
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click();
 
 
 
